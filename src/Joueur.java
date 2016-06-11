b77eb0f93a407e616cf52703fb8f4e6563dee1a6
PixCYan <anaisraffenne@gmail.com>
2016-06-11 16:10:08 +0200

    Initial commit

---
+++ src/Joueur.java
@@ -0,0 +1,11 @@
+/**
+ * Created by raffennn on 11/06/2016.
+ * Gestion d'un joueur
+ */
+public class Joueur {
+    private int id;
+    private String pseudo;
+    private String password;
+    private String totalScore;
+    private Partie partie;
+}
---
+++ src/Partie.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+/**
+ * Created by raffennn on 11/06/2016.
+ * Gestion d'une partie
+ */
+public class Partie {
+    private int id;
+    private int nbJoueurs;
+    private int nbCarte;
+    private LinkedList<Joueur> joueurs;
+}
