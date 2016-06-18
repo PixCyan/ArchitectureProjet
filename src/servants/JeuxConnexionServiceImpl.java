b75d09672bdc479567ef0e26ef7caf2cdec9c381
jeremiehiyoko <jeremie.berthillot@gmail.com>
2016-06-18 15:26:45 +0200

    debut du client RMI sur le serveur logique

--- src/servants/JeuxConnexionServiceImpl.java
+++ src/servants/JeuxConnexionServiceImpl.java
@@ -3,6 +3,7 @@ package servants;
 import Jeux.IConnexionRemotePOA;
 import Jeux.Joueur;
 
+
 /**
  * Created by Utilisateur on 13/06/2016.
  */
@@ -11,6 +12,7 @@ public class JeuxConnexionServiceImpl extends IConnexionRemotePOA {
     @Override
     public boolean seConnecter(String pseudo, String mdp) {
         return false;
+
     }
 
     @Override
